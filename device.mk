311c570b212ecc5eda3228efe8eb938dfd5a6589
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-17 04:59:30 +0300

    eqe: Do not manually build dependencies
    Change-Id: I4046d8eeabfc70842898b7034f75cdd6bcfc441b

--- device.mk
+++ device.mk
@@ -264,10 +264,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2.vendor \
-    android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
-    libchrome.vendor \
     Tag \
     nfc_nci.st21nfc.default
 
