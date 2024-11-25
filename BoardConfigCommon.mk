LineageOS/android_device_oneplus_sm8550-common

sm8550-common: Work around using prebuilt audio primary HAL
Since audio.primary.kalama is no longer present in PRODUCT_PACKAGES,
the vintf files are not copied anymore, so account for that and merge
to the manifest instead.

Change-Id: I71b091950e5ecc810779d3ab51e99807d6648d0b

BoardConfigCommon.mk
@@ -54,6 +54,7 @@ AUDIO_FEATURE_ENABLED_SVA_MULTI_STAGE := true
 BOARD_SUPPORTS_OPENSOURCE_STHAL := true
 BOARD_SUPPORTS_SOUND_TRIGGER := true
 BOARD_USES_ALSA_AUDIO := true
+TARGET_PROVIDES_AUDIO_HAL := true
 TARGET_USES_QCOM_MM_AUDIO := true
 
 # Bootloader
@@ -93,7 +94,10 @@ DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/framework_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/manifest.xml \
-    $(COMMON_PATH)/network_manifest.xml
+    $(COMMON_PATH)/network_manifest.xml \
+    hardware/qcom-caf/sm8550/audio/primary-hal/configs/common/manifest_non_qmaa.xml \
+    hardware/qcom-caf/sm8550/audio/primary-hal/configs/common/manifest_non_qmaa_extn.xml
+
 ODM_MANIFEST_FILES := \
     $(COMMON_PATH)/manifest_odm.xml \
     $(COMMON_PATH)/network_manifest_odm.xml

moto-sm7550-devs/android_device_motorola_eqe

BoardConfigCommon.mk
@@ -94,6 +94,7 @@ AUDIO_FEATURE_ENABLED_PAL_HIDL := true
 AUDIO_FEATURE_ENABLED_PROXY_DEVICE := true
 BOARD_SUPPORTS_SOUND_TRIGGER := true
 BOARD_USES_ALSA_AUDIO := true
+TARGET_PROVIDES_AUDIO_HAL := true
 TARGET_USES_QCOM_MM_AUDIO := true
 USE_CUSTOM_AUDIO_POLICY := 1
 
@@ -118,7 +119,10 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_SKUS := kalama
-DEVICE_MANIFEST_KALAMA_FILES += $(COMMON_PATH)/manifest_kalama.xml
+DEVICE_MANIFEST_KALAMA_FILES += \
+    $(COMMON_PATH)/manifest_kalama.xml \
+    hardware/qcom-caf/sm8550/audio/primary-hal/configs/common/manifest_non_qmaa.xml \
+    hardware/qcom-caf/sm8550/audio/primary-hal/configs/common/manifest_non_qmaa_extn.xml
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true