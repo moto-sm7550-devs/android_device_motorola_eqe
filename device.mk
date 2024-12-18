da775396310df2806755879bfcd3d0523523f090
Venkat Yadlapati <venkat.yadlapati@hotmail.com>
2024-12-19 02:42:39 +0530

    eqe: Build st21nfc
    Change-Id: Ife9b9eef362c117cf1911e7af4a310fc5900284c

--- device.mk
+++ device.mk
@@ -248,11 +248,12 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2.vendor \
+    android.hardware.nfc@1.2-service.st \
     android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
     libchrome.vendor \
-    nqnfcinfo \
-    Tag
+    Tag \
+    nfc_nci.st21nfc.default
 
 # Permissions
 PRODUCT_COPY_FILES += \
