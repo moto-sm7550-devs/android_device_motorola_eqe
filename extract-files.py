448e38438237c44d29b9b3400ec5a7fd56c9fc64
SamarV-121 <samar@samarv121.dev>
2025-01-31 11:07:46 +0530

    eqe: Include firmware and carriersettings
    Change-Id: I0272fc10472b84badbcd684cd3e7b7e28d827ada

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.extract import extract_fns_user_type
+from extract_utils.extract_star import extract_star_firmware
 from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
@@ -90,12 +92,19 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libhidlbase_shim.so'),
 }  # fmt: skip
 
+extract_fns: extract_fns_user_type = {
+    r'(bootloader|radio)\.img': extract_star_firmware,
+}
+
 module = ExtractUtilsModule(
     'eqe',
     'motorola',
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
+    extract_fns=extract_fns,
+    add_firmware_proprietary_file=True,
+    add_generated_carriersettings=True,
 )
 
 if __name__ == '__main__':
