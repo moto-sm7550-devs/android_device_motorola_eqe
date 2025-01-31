46b06d58472059aa7a35ce83800c9c05db1b875e
lahaina <nattolecats@gmail.com>
2025-01-31 18:34:23 +0900

    eqe: Switch to prebuilt st21nfc service * For enabling Felica feature, it require HAL version "ST21NFC HAL1.3C", which is not provided on OSS.   so let's bring back prebuilt for now.
    This partially reverts commit 0177b1fc0558758c9ed290804f8f673c88c99c12.

--- device.mk
+++ device.mk
@@ -270,7 +270,6 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2.vendor \
-    android.hardware.nfc@1.2-service.st \
     android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
     libchrome.vendor \
