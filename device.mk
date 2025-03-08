72c5cf1b539f6349cb293fe7e4c30f1b03aa0c49
Michael Bestas <mkbestas@gmail.com>
2025-03-08 21:43:32 +0200

    eqe: Enable ELF checks for libwpa_client dependent libs
    Change-Id: I490df02b68da4b9a7088a239672a1dcace66d270

--- device.mk
+++ device.mk
@@ -450,7 +450,6 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
     libqsap_sdk \
-    libwpa_client \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
     wpa_supplicant \
--- extract-files.py
+++ extract-files.py
@@ -34,7 +34,6 @@ namespace_imports = [
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
     return f'{lib}_{partition}' if partition == 'vendor' else None
 
-
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
@@ -55,7 +54,6 @@ lib_fixups: lib_fixups_user_type = {
         'libagmclient',
         'libpalclient',
         'libqsap_sdk',
-        'libwpa_client',
     ): lib_fixup_remove,
 }
 
