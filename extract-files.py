32e7085a0f1aa59258f4557dcc5335c8006e6e33
Michael Bestas <mkbestas@lineageos.org>
2025-03-09 12:45:54 +0200

    eqe: Shim sensors.moto with libbase_shim
    See: https://cs.android.com/android/_/android/platform/system/libbase/+/46fc805282265715266c256fbe34cf1a886c475f
    
    Change-Id: I5127020e18114a13515d9dc382c95d689634243d

--- extract-files.py
+++ extract-files.py
@@ -84,6 +84,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libhidlbase_shim.so'),
     ('vendor/lib/libmot_chi_desktop_helper.so', 'vendor/lib64/libmot_chi_desktop_helper.so'): blob_fixup()
         .add_needed('libgui_shim_vendor.so'),
+    'vendor/lib64/sensors.moto.so': blob_fixup()
+        .add_needed('libbase_shim.so'),
     (
         'vendor/lib64/libqcrilNr.so',
         'vendor/lib64/libril-db.so',
