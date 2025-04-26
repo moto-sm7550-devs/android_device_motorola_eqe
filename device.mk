04a3e63975ea55a119c55902c147bb3a041f8b08
Marc Bourgoin <themard85@gmail.com>
2025-04-26 12:09:54 -0600

    eqe: Switch to CLO audio effects config
    Also remove no longer used soundfx blobs
    
    This fixes Bluetooth Audio crashing when toggling HD Audio on/off
    
    Change-Id: I03393c855c99fecad82af4b28b6586c563c510c1

--- device.mk
+++ device.mk
@@ -115,8 +115,7 @@ PRODUCT_PACKAGES += \
 AUDIO_HAL_DIR := hardware/qcom-caf/sm8550/audio/primary-hal
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
-    $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_crow/audio_effects.xml \
+    $(AUDIO_HAL_DIR)/configs/crow/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_crow/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_crow_qssi/audio_policy_configuration.xml
 
