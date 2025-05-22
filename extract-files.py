c87607d258f855706d3bd4b6a02827fe5252b15a
Sarthak Roy <sarthakroy2002@gmail.com>
2025-05-22 05:13:29 -0600

    eqe: Add patches for a15 ims package
    Change-Id: I7f31bd0c34e7d60d082ab431cdc704b87d3da4f9

--- extract-files.py
+++ extract-files.py
@@ -64,8 +64,8 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'system_ext/etc/permissions/moto-telephony.xml': blob_fixup()
         .regex_replace('/system/', '/system_ext/'),
-   # 'system_ext/priv-app/ims/ims.apk': blob_fixup()
-    #    .apktool_patch('ims-patches'),
+    'system_ext/priv-app/ims/ims.apk': blob_fixup()
+        .apktool_patch('ims-patches'),
     'vendor/lib64/libwvhidl.so': blob_fixup()
         .add_needed('libcrypto_shim.so'),
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
